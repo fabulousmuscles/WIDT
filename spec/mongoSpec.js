76e9e22ad6ea69ca836373f3b76ba1f5b38e574c
Reuben Urbina <reubenurbina@gmail.com>
2014-03-10 17:09:14 -0700

    another test in spec/mongoSpec.js

--- spec/mongoSpec.js
+++ spec/mongoSpec.js
@@ -1,34 +1,48 @@
 var mongoose = require('mongoose');
 var Entry = require('../app/models/entry')
 
-describe("Create an entry", function() {
+describe("mongodb database", function() {
+
     beforeEach(function() {
         mongoose.connect('mongodb://localhost/testdb');
     });
 
     afterEach(function() {
-        mongoose.connection.db.executeDbCommand({
-            dropDatabase: 1
-        }, function(err, result) {
-            console.log(err);
-            console.log(result);
-            process.exit(0);
+        //  how to drop db after all tests complete???
+        mongoose.connection.close();
+        // mongoose.connection.db.executeDbCommand({
+        //     dropDatabase: 1
+        // }, function(err, result) {
+        //     console.log(err);
+        //     console.log(result);
+        //     process.exit(0);
+        // });
+    });
+
+    describe("creating an entry", function() {
+        // note the 'done' callback: this is an async test
+        it('should save entries', function(done) {
+            Entry.create({text : 'foo'}, function(err, entry) {
+                    expect(entry.text).toBe('foo');
+                    expect(entry.created_at).toBeDefined();
+                    expect(entry.modified).toBeDefined();
+                    done();
+            });
+            Entry.create({text : 'bar'}, function(err, entry) {
+                    expect(entry.text).toBe('bar');
+                    expect(entry.created_at).toBeDefined();
+                    expect(entry.modified).toBeDefined();
+                    done();
+            });
         });
     });
 
-    // note the 'done' callback: this is an async test
-    it('should save to the database', function(done) {
-        Entry.create({text : 'hello'}, function(err, entry) {
-                expect(entry.text).toBe('hello');
-                expect(entry.created_at).toBeDefined();
-                expect(entry.modified).toBeDefined();
+    describe("querying the database", function() {
+        it('should retrieve entries', function(done) {
+            Entry.find(function(err, entries) {
+                expect(entries.length).toEqual(2);
                 done();
+            });
         });
-    });
-});
-
-describe("A suite", function() {
-    it("contains spec with an exception", function() {
-        expect(true).toBe(true);
     });
 });

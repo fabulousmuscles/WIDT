c27e124c697f89bbbc428762f65b8a01801f319b
Reuben Urbina <reubenurbina@gmail.com>
2014-03-13 23:31:44 -0700

    finished nodeSpec e2e tests

--- spec/nodeSpec.js
+++ spec/nodeSpec.js
@@ -1,9 +1,7 @@
 var request = require('request');
-var Entry = require('../app/models/entry');
 
 describe("The homepage", function() {
     it("should respond with widt", function(done) {
-        console.log(process.env.NODE_ENV);
         request("http://localhost:8000", function(error, response, body) {
             expect(response.statusCode).toBe(200);
             expect(body).toContain('WIDT');
@@ -15,30 +13,85 @@ describe("The homepage", function() {
 describe("The api", function() {
 
     it("should respond with an empty array if no entries", function(done) {
-        request("http://localhost:8000/api/entries", function(error, response, body) {
+        request("http://localhost:8000/api/entries", function(err, res, body) {
+            expect(err).toBeNull();
             expect(body).toBe('[]');
             done();
         });
     });
 
-    var options = {
-        url: "http://localhost:8000/api/entries",
-        form: {
-            text: "hello world!"
-        }
-    }
     it("should create an entry", function(done) {
-        request.post(options, function(error, response, body) {
+        var options = {
+            url: "http://localhost:8000/api/entries",
+            form: {text: "foo"}
+        }
+        request.post(options, function(err, res, body) {
+            expect(err).toBeNull();
+            expect(body).toContain('foo')
             done();
         });
     });
 
-    it("should respond with an array of entries if there are any", function(done) {
-        request("http://localhost:8000/api/entries", function(error, response, body) {
-            expect(body).toContain('hello world!');
+    it("should respond with an entry if there is one", function(done) {
+        request("http://localhost:8000/api/entries", function(err, res, body) {
+            expect(err).toBeNull();
+            expect(body).toContain('foo');
+            expect(body).toNotContain('bar');
             done();
         });
     });
 
-    //it("should delete an entry" function(done) {
+    it("should create another entry", function(done) {
+        var options2 = {
+            url: "http://localhost:8000/api/entries",
+            form: {text: "bar"}
+        }
+        request.post(options2, function(err, res, body) {
+            expect(err).toBeNull();
+            expect(body).toContain('bar')
+            done();
+        });
+    });
+
+    it("should respond with an array of entries", function(done) {
+        request("http://localhost:8000/api/entries", function(err, res, body) {
+            expect(err).toBeNull();
+            expect(body).toContain('foo');
+            expect(body).toContain('bar');
+            done();
+        });
+    });
+
+    it("should delete an entry", function(done) {
+        var re = /_id": "(.*?)"/g
+        request("http://localhost:8000/api/entries", function(err, res, body) {
+            expect(err).toBeNull();
+            // get the entry ids
+            id1 = body.match(re)[0].replace('_id": ', '').replace(/\"/g, '');
+            id2 = body.match(re)[1].replace('_id": ', '').replace(/\"/g, '');
+            var options1 = {url: "http://localhost:8000/api/entries/" + id1}
+            // delete the first entry
+            request.del(options1, function(err, res, body) {
+                expect(err).toBeNull();
+                var options2 = {url: "http://localhost:8000/api/entries/" + id2}
+                // delete the second entry
+                request.del(options2, function(err, res, body) {
+                    request("http://localhost:8000/api/entries", function(err, res, body) {
+                        expect(err).toBeNull();
+                        expect(body).toNotContain('foo');
+                        expect(body).toNotContain('bar');
+                        done();
+                    });
+                });
+            });
+        });
+    });
+
+    it("be empty", function(done) {
+        request("http://localhost:8000/api/entries", function(err, res, body) {
+            expect(err).toBeNull();
+            expect(body).toBe('[]');
+            done();
+        });
+    });
 });

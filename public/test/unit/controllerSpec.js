8e16a847fdbe295034a80d6d3aa10960a4c0f075
Reuben Urbina <reubenurbina@gmail.com>
2014-03-16 01:11:02 -0700

    controllerSpec: added PUT test and started testing categories

--- public/test/unit/controllerSpec.js
+++ public/test/unit/controllerSpec.js
@@ -1,6 +1,5 @@
 'use strict';
                                
-/* jasmine specs for controllers */
 describe('WIDT controllers', function() {
 
   beforeEach(function(){       
@@ -21,7 +20,10 @@ describe('WIDT controllers', function() {
       $httpBackend = _$httpBackend_;  
 
       $httpBackend.expectGET('api/entries').
-          respond([{text: 'office fun', _id: '222'}, {text: 'stood by the watercooler', _id: '333'}]);
+          respond([
+              {text: 'office fun', _id: '222'}, 
+              {text: 'stood by the watercooler', _id: '333'}
+          ]);
 
       $httpBackend.expectGET('api/categories').
           respond([{text: 'fun', _id: '123'}, {text: 'news', _id: '456'}]);
@@ -35,12 +37,10 @@ describe('WIDT controllers', function() {
       expect(scope.entries).toEqualData([]);
       $httpBackend.flush();
 
-      expect(scope.entries).toEqualData(
-          [
-            {text: 'office fun', _id: '222'}, 
-            {text: 'stood by the watercooler', _id: '333'}
-          ]
-      );
+      expect(scope.entries).toEqualData([
+          {text: 'office fun', _id: '222'},
+          {text: 'stood by the watercooler', _id: '333'}
+      ]);
     });
 
     it('should display 2 categories fetched from xhr', function() {
@@ -73,6 +73,29 @@ describe('WIDT controllers', function() {
         expect(scope.entries.length).toBe(1);
         expect(scope.entries).toEqualData(
           [{text: 'stood by the watercooler', _id: '333'}]);
+    });
+
+    it('should update an entry', function() {
+        // Angular sets the 'modified' property, the 'text' property
+        // is updated on the backend only
+        var entry = {text: 'office fun', _id: '222', modified: 'no'}; 
+        var entry2 = {text: 'office fun', _id: '222', modified: 'yes'}; 
+        $httpBackend.expectPUT('api/entries', entry).respond(200, entry2);
+        scope.updateEntry(entry);
+        $httpBackend.flush();
+        expect(entry).toEqualData(entry2);
+    });
+
+    it('should save a "global" category', function() {
+        $httpBackend.expectPOST('api/categories', 'cool').respond(
+            200, {text: 'cool'}
+        );
+        scope.addEntryCategory('cool')
+        $httpBackend.flush();
+        expect(scope.categories[scope.categories.length - 1].text).toBe('cool');
+        expect(scope.categories[scope.categories.length - 1].$resolved).toBe(true);
+        expect(scope.categories.length).toBe(3);
+        // console.log(scope.entryCategories);
     });
   });
 });

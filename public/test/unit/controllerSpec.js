f534d52fa350f9a1fced279a560991d763ab77d8
Reuben Urbina <reubenurbina@gmail.com>
2014-03-15 21:23:00 -0700

    delete entry test in controllerSpec

--- public/test/unit/controllerSpec.js
+++ public/test/unit/controllerSpec.js
@@ -21,10 +21,10 @@ describe('WIDT controllers', function() {
       $httpBackend = _$httpBackend_;  
 
       $httpBackend.expectGET('api/entries').
-          respond([{text: 'office fun'}, {text: 'stood by the watercooler'}]);
+          respond([{text: 'office fun', _id: '222'}, {text: 'stood by the watercooler', _id: '333'}]);
 
       $httpBackend.expectGET('api/categories').
-          respond([{text: 'fun'}, {text: 'news'}]);
+          respond([{text: 'fun', _id: '123'}, {text: 'news', _id: '456'}]);
       
       scope = $rootScope.$new();      
       ctrl = $controller('MainCtrl', {$scope: scope});
@@ -36,7 +36,11 @@ describe('WIDT controllers', function() {
       $httpBackend.flush();
 
       expect(scope.entries).toEqualData(
-          [{text: 'office fun'}, {text: 'stood by the watercooler'}]);
+          [
+            {text: 'office fun', _id: '222'}, 
+            {text: 'stood by the watercooler', _id: '333'}
+          ]
+      );
     });
 
     it('should display 2 categories fetched from xhr', function() {
@@ -44,11 +48,13 @@ describe('WIDT controllers', function() {
       $httpBackend.flush();
 
       expect(scope.categories).toEqualData(
-          [{text: 'fun'}, {text: 'news'}]);
+          [{text: 'fun', _id: '123'}, {text: 'news', _id: '456'}]);
     });
 
     it('should save an entry', function() {
-        $httpBackend.expectPOST('api/entries', 'watched anime').respond(200, {text: 'watched anime'});
+        $httpBackend.expectPOST('api/entries', 'watched anime').respond(
+            200, {text: 'watched anime'}
+        );
         scope.addEntry('watched anime')
         $httpBackend.flush();
         expect(scope.entries[0].text).toBe('watched anime');
@@ -57,6 +63,16 @@ describe('WIDT controllers', function() {
         expect(scope.newEntry).toBe('');
         expect(scope.entryCategories).toEqualData([]);
         expect(scope.selectCategory).toEqualData(null);
+    });
+
+    it('should delete an entry', function() {
+        $httpBackend.whenDELETE('api/entries/222').respond(200, '222');
+        $httpBackend.flush();
+        expect(scope.entries.length).toBe(2);
+        scope.deleteEntry('222');
+        expect(scope.entries.length).toBe(1);
+        expect(scope.entries).toEqualData(
+          [{text: 'stood by the watercooler', _id: '333'}]);
     });
   });
 });
